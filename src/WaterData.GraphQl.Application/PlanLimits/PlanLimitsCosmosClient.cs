34116c980a86e1e4497a033f15eab7ecef8ab99c
Marc Stevenson <marcsstevenson@hotmail.com>
2020-07-23 14:18:15 +1200

    Added Cosmos link with testing

---
+++ src/WaterData.GraphQl.Application/PlanLimits/PlanLimitsCosmosClient.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace WaterData.GraphQl.Application.PlanLimits
+{
+    /// <summary>
+    /// The purpose of this class is to provide connectivity to a Plan Limits Cosmos database
+    /// </summary>
+    public class PlanLimitsCosmosClient
+    {
+        public async Task<int> Go(ILogger logger)
+        {
+            CosmosClient client = new CosmosClient(
+                accountEndpoint: "",
+                authKeyOrResourceToken: "");
+
+            Database database = await client.CreateDatabaseIfNotExistsAsync("TeamGraph");
+            Container container = await database.CreateContainerIfNotExistsAsync(
+                "PlanLimitData",
+                "/id",
+                400);
+
+            var resultCount = 0;
+            var sql = "select * from C";
+
+
+            // Query for an item
+            using (FeedIterator<dynamic> feedIterator = container.GetItemQueryIterator<dynamic>(sql))
+            {
+                while (feedIterator.HasMoreResults)
+                {
+                    FeedResponse<dynamic> response = await feedIterator.ReadNextAsync();
+                    foreach (var item in response)
+                    {
+                        resultCount++;
+                        string str = item.ToString();
+                        logger.LogDebug(str);
+                    }
+                }
+            }
+
+            return resultCount;
+        }
+    }
+}
---
+++ src/WaterData.GraphQl.Functions/Testing.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Threading.Tasks;
+using WaterData.GraphQl.Application.PlanLimits;
+using Microsoft.Extensions.Logging;
+
+namespace WaterData.GraphQl.Functions
+{
+    public class Testing
+    {
+        /// <summary>
+        /// Returns 200 if our site configuration is valid or 500 is not
+        /// </summary>
+        /// <returns>An action result</returns>
+        [FunctionName("Testing")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger logger)
+        {
+            var client = new PlanLimitsCosmosClient();
+
+            var resultCount = await client.Go(logger);
+
+            return new OkObjectResult(new { resultCount });
+        }
+    }
+}

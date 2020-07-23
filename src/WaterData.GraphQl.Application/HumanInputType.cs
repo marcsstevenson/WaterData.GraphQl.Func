86e95d2ef1128c8cfc330c51c9e8e9118ff906f5
Marc Stevenson <marcsstevenson@hotmail.com>
2020-07-23 13:08:50 +1200

    Added example files

---
+++ src/WaterData.GraphQl.Application/HumanInputType.cs
@@ -0,0 +1,15 @@
+using GraphQL.Types;
+using WaterData.GraphQl.Application.Types;
+
+namespace WaterData.GraphQl.Application
+{
+    public class HumanInputType : InputObjectGraphType<Human>
+    {
+        public HumanInputType()
+        {
+            Name = "HumanInput";
+            Field(x => x.Name);
+            Field(x => x.HomePlanet, nullable: true);
+        }
+    }
+}
---
+++ src/WaterData.GraphQl.Application/StarWarsSchema.cs
@@ -0,0 +1,16 @@
+using GraphQL.Types;
+using GraphQL.Utilities;
+using System;
+
+namespace WaterData.GraphQl.Application
+{
+    public class StarWarsSchema : Schema
+    {
+        public StarWarsSchema(IServiceProvider provider)
+            : base(provider)
+        {
+            Query = provider.GetRequiredService<StarWarsQuery>();
+            Mutation = provider.GetRequiredService<StarWarsMutation>();
+        }
+    }
+}
